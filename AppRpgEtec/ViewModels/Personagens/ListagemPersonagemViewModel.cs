b8513375c9c4dae4269691e10b1a72d878edd7fd
EmyVi <EmyVi@exemplo>
2023-05-15 09:28:44 -0300

    primeiro commit

---
+++ AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -0,0 +1,34 @@
+﻿using AppRpgEtec.Models;
+using AppRpgEtec.Services.Personagns;
+using System.Collections.ObjectModel;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class ListagemPersonagemViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+        public ObservableCollection<Personagem> Personagens { get; set; }
+        public ListagemPersonagemViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken",  string.Empty);
+            pService= new PersonagemService(token);
+            Personagens= new ObservableCollection<Personagem>();
+
+            _ = ObterPersonagens();
+        }
+
+        public async Task ObterPersonagens()
+        {
+            try
+            {
+                Personagens = await pService.GetPersonagensAsync();
+                OnPropertyChanged(nameof(Personagens));
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+    }
+}
